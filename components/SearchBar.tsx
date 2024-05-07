959fd556a0a06eb8868da188be14ca96ba072969
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2024-05-07 23:40:25 +0200

    style the alert modal a bit

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, Fragment } from "react";
 import Image from "next/image";
-import { SearchManufacturer, SearchButton } from "@/components";
+import { CustomButton, SearchManufacturer, SearchButton } from "@/components";
 import { Dialog, Transition } from "@headlessui/react";
 
 interface SearchBarProps {
@@ -86,6 +86,19 @@ export const SearchBar = ({ setManufacturer, setModel }: SearchBarProps) => {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <button
+                    type="button"
+                    onClick={() => setIsModalOpen(false)}
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-3xl"
+                  >
+                    <Image
+                      src="/close.svg"
+                      alt="close"
+                      width={20}
+                      height={20}
+                      className="object-contain"
+                    ></Image>
+                  </button>
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
@@ -99,13 +112,13 @@ export const SearchBar = ({ setManufacturer, setModel }: SearchBarProps) => {
                   </div>
 
                   <div className="mt-4">
-                    <button
+                    <CustomButton
+                      title="Got it"
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={() => setIsModalOpen(false)}
-                    >
-                      Got it
-                    </button>
+                      containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+                      textStyles="text-white text-[14px] leading-[17px] font-bold"
+                      handleClick={() => setIsModalOpen(false)}
+                    />
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
